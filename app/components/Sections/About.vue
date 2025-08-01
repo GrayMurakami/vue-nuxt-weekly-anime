5c772f547ec579bc3e0d255de64a24943306cfdf
GraY <125752802+GrayMurakami@users.noreply.github.com>
2025-08-01 16:07:10 +0900

    Update About.vue

--- app/components/Sections/About.vue
+++ app/components/Sections/About.vue
@@ -31,28 +31,26 @@
 <template>
   <div class="about-me">
     <section id="about">
-    <h1 class="section-text">About</h1>
-    <div class="about-center">
-        <div class="col-md-10">
+      <h1 class="section-text">About</h1>
         <div class="about-center">
+          <div class="col-md-10">
+            
+            <div class="about-center">
+              <div class="about-row col-md-1">
+                <div class="photo-move">
+                  <img src="/image/biography.jpg" class="about-photo" alt="Photo">
+                </div>
+              </div>
 
-            <div class="about-row col-md-1">
-            <div class="photo-move">
-                <img src="/image/biography.jpg" class="about-photo" alt="Photo">
+              <div class="about-row col-md-2">
+                <div class="about-text">
+                  Programmer, designer, sportsman and just a positive person, who get a lot of experience in his life! This is one of the examples of my designer work. I am constantly developing and learning something new, just in one year I found a job in IT from scratch and gained many qualifications in both frontend and backend sphere. Creating sites is very interesting and entertaining for me. Maybe I am not a master in this business, but someday it will definitely become. My strong point is willpower, which helps me to achieve everything I want.!
+                  <div class="about-text-do">Let's Do It!</div>
+                </div>
+              </div>
             </div>
-            </div>
-
-            <div class="about-row col-md-2">
-            <div class="about-text">
-                Programmer, designer, sportsman and just a positive person, who get a lot of experience in his life! This is one of the examples of my designer work. I am constantly developing and learning something new, just in one year I found a job in IT from scratch and gained many qualifications in both frontend and backend sphere. Creating sites is very interesting and entertaining for me. Maybe I am not a master in this business, but someday it will definitely become. My strong point is willpower, which helps me to achieve everything I want.!
-                <div class="about-text-do">Let's Do It!</div>
-            </div>
-            </div>
-        
+          </div>
         </div>
-        </div>
-    </div>
-
     </section>
   </div>
 </template>
@@ -105,7 +103,8 @@
 .photo-move {
   perspective: 1000px;
   display: inline-block; 
-    & > img  {
+  
+  & > img  {
     height: auto;
     transform: rotateX(var(--mouseY)) rotateY(var(--mouseX)); /* Используем CSS-переменные */
     transition: transform 0.1s ease-out; /* Плавный переход */
